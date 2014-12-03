16c78e57456b2871664ab9ab419a56f54861ceaf
Figglewatts <figglewatts@hotmail.co.uk>
2014-12-03 16:07:39 +0000

    Added gson library for json serialization

--- src/figglewatts/lsdrsdk/SeqCreate/SeqCreate.java
+++ src/figglewatts/lsdrsdk/SeqCreate/SeqCreate.java
@@ -1,10 +1,25 @@
 package figglewatts.lsdrsdk.SeqCreate;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class SeqCreate {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("SeqCreate - Created by Figglewatts, 2014");
+		System.out.println("For use with LSD: Revamped SDK");
+		
+		// create writer to write to a json file
+		try (Writer writer = new OutputStreamWriter(new FileOutputStream("output.json"), "UTF-8")) {
+			// create instance of gson
+			Gson gson = new GsonBuilder().create();
+		}
+		
 	}
 
 }
